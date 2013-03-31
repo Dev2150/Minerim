6b1c3e600db2db13a045f2b57cb57d50bf25b2ff
myiume <cursed_kidd@yahoo.com>
2013-03-31 22:19:13 +0300

    Update Minerim.java

--- common/myiume/mods/minerim/Minerim.java
+++ common/myiume/mods/minerim/Minerim.java
@@ -28,3 +28,47 @@ import cpw.mods.fml.common.registry.LanguageRegistry;
 @Mod(modid = "Minerim", name = "Minerim", dependencies = "required-after:Forge@[7.0,);required-after:FML@[5.0.5,)")
 @NetworkMod(channels = { "Minerim" }, versionBounds = "[0.1,)", clientSideRequired = true, serverSideRequired = false, packetHandler = PacketHandler.class)
 public class Minerim {
+  public static BlockMinerimRock minerimRockBlock;
+  @SidedProxy(clientSide = "myiume.mods.minerim.client.ClientProxy", serverSide = "myiume.mods.minerim.CommonProxy")
+  public static CommonProxy proxy;
+  @Instance("Minerim")
+  public static Minerim instance;
+  private int blockId;
+  
+  @PreInit
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        Version.init(event.getVersionProperties());
+        event.getModMetadata().version = Version.fullVersionString();
+        Configuration cfg = new Configuration(event.getSuggestedConfigurationFile());
+        try
+        {
+            cfg.load();
+            blockId = cfg.getBlock("minerimRock", 975).getInt(975);
+            ChestChangerType.buildItems(cfg, 19501);
+        }
+        catch (Exception e)
+        {
+            FMLLog.log(Level.SEVERE, e, "Minerim has a problem loading it's configuration");
+        }
+        finally
+        {
+            cfg.save();
+        }
+    }
+
+    @Init
+    public void load(FMLInitializationEvent evt)
+    {
+        minerimRockBlock = new BlockMinerimRock(blockId);
+        GameRegistry.registerBlock(minerimRockBlock, ItemMinerimRock.class, "BlockMinerimRock");
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @PostInit
+    public void modsLoaded(FMLPostInitializationEvent evt)
+    {
+    }
+
+}
+  
